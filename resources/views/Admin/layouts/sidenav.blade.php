4db2964e14d4e140203ef114d9868dac49670c5a
DheoAp <dheoap21@gmail.com>
2021-06-03 16:18:19 +0700

    menambahkan error page dan ubah nama menu sidebar

--- resources/views/Admin/layouts/sidenav.blade.php
+++ resources/views/Admin/layouts/sidenav.blade.php
@@ -8,58 +8,42 @@
             Dashboard
         </a>
 
-        <div class="sb-sidenav-menu-heading">Interface</div>
+        <div class="sb-sidenav-menu-heading">Info</div>
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
             <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-            Layouts
+            Akademik
             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
         </a>
         <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
           <nav class="sb-sidenav-menu-nested nav">
-            <a class="nav-link" href="layout-static.html">Static Navigation</a>
-            <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+            <a class="nav-link" href="layout-static.html">Mata Pelajaran</a>
+            <a class="nav-link" href="layout-static.html">Siswa</a>
+            <a class="nav-link" href="layout-static.html">Tahun Ajaran</a>
+            <a class="nav-link" href="layout-static.html">Input Nilai</a>
+            <a class="nav-link" href="layout-static.html">Cetak Nilai</a>
+            <a class="nav-link" href="layout-static.html">Guru</a>
           </nav>
         </div>
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-          <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-          Pages
-          <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePengaturan" aria-expanded="false" aria-controls="collapsePengaturan">
+            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+            Pengaturan
+            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
         </a>
-        <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
-          <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-              <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                  Authentication
-                  <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-              </a>
-              <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
-                  <nav class="sb-sidenav-menu-nested nav">
-                      <a class="nav-link" href="login.html">Login</a>
-                      <a class="nav-link" href="register.html">Register</a>
-                      <a class="nav-link" href="password.html">Forgot Password</a>
-                  </nav>
-              </div>
-              <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                Error
-                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-              </a>
-              <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
-                <nav class="sb-sidenav-menu-nested nav">
-                  <a class="nav-link" href="401.html">401 Page</a>
-                  <a class="nav-link" href="404.html">404 Page</a>
-                  <a class="nav-link" href="500.html">500 Page</a>
-                </nav>
-              </div>
+        <div class="collapse" id="collapsePengaturan" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+          <nav class="sb-sidenav-menu-nested nav">
+            <a class="nav-link" href="layout-static.html">User</a>
+            <a class="nav-link" href="layout-static.html">Menu</a>
           </nav>
         </div>
-
-        <div class="sb-sidenav-menu-heading">Addons</div>
+        
+        <div class="sb-sidenav-menu-heading">Lainnya</div>
         <a class="nav-link" href="charts.html">
           <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-          Charts
+          Info Sekolah
         </a>
         <a class="nav-link" href="tables.html">
           <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-          Tables
+          Kontak
         </a>
       </div>
     </div>
